94f41a4a3e15e3b12f4b39c9e2acd25c1c684122
lucasmbispo <lucasapex@gmail.com>
2022-04-09 17:07:14 -0300

    v2

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const getTableData = async (): Promise<Array<any>> => {
     const $ = cheerio.load(pageResponse.data);
 
 
-    //find table content, search in all lines//
+    //find table content, search in all rows//
     $("table.wikitable.sortable")
     .find("tr")
     .each((row, elem) => {
@@ -30,8 +30,7 @@ const getTableData = async (): Promise<Array<any>> => {
             $(elem).find('th').each((idx, elem) => {
 
                 //get the column title
-                const key = $(elem).text().trim();
-
+                const key = $(elem).text().trim().replace('\n','');
                 //ignore the columns I don't want
                 if(idx !== 6 && idx !== 9 && idx !== 10)
 
@@ -43,6 +42,7 @@ const getTableData = async (): Promise<Array<any>> => {
 
             });
 
+            //create and set Year column
             keys[10] = 'Year'
 
 
@@ -54,11 +54,12 @@ const getTableData = async (): Promise<Array<any>> => {
            
         const nextCountry = {};
         
-
+        //for each column
         $(elem).find('td').each((idx, elem) => {
 
             const value = $(elem).text().trim().replace(',','.');
-            
+
+            //ignore the columns I don't want
             if(idx !== 6 && idx !== 9 && idx !== 10){
 
                 nextCountry[keys[idx]] = value.replace(',','.');
@@ -75,7 +76,10 @@ const getTableData = async (): Promise<Array<any>> => {
 
     //return results//
 
+    //remove the last row (totals)
     const list = result.slice(0,-1);
+
+    //return list with sort applied
     return sortByKey(list, 7);
     
 };
@@ -88,18 +92,27 @@ const getTableData = async (): Promise<Array<any>> => {
 const saveCsv = async (countries: Array<any>) => {
     //console.info(`Saving ${countries.length} records`);
 
+
+//create parser
 const j2cp = new json2csv.Parser();
 const csv = j2cp.parse(countries);
 
+
+//write file
 await fsp.writeFile("./output.csv", csv, {encoding: "utf-8"});
+
 }
+
+//execute function then write file
 getTableData().then(saveCsv);  
+
+//sort function
 
 function sortByKey(array, key) {
     return array.sort((a, b) => {
-      let x = a['Road deaths\nper Million Inhabitants in 2018[27]'];
+      let x = a['Road deathsper Million Inhabitants in 2018[27]'];
       console.log(x)
-      let y = b['Road deaths\nper Million Inhabitants in 2018[27]'];
+      let y = b['Road deathsper Million Inhabitants in 2018[27]'];
       console.log(y)
       return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
